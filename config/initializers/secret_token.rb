f1ee0c69367508331b8bf5ffa1e77da916704b44
Evan Sparkman <esparkman@esdezines.com>
2012-09-23 17:37:20 -0400

    Initial App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubunity::Application.config.secret_token = '5f34b174c255b22451823836021b45255fc077324f6f76a08cfd267b973c0af9fba61abc2b48443cf8ee7a91f924c3516ebe7645bb82ee706aac76e735e2869b'
